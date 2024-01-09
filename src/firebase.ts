65b6008af34a48fa279f1e85999f9457c93dc427
hansol <hansol8701@naver.com>
2024-01-09 16:30:20 +0900

    챕터 3

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAdruhS5C0N_pisiC3dgefb_j2SQQizuUE",
+    authDomain: "nwitter-reloaded-93e46.firebaseapp.com",
+    projectId: "nwitter-reloaded-93e46",
+    storageBucket: "nwitter-reloaded-93e46.appspot.com",
+    messagingSenderId: "118315011843",
+    appId: "1:118315011843:web:b86b0105273cde801d2336",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
